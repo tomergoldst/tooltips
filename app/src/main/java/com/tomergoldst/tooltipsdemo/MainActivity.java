f64710398b987480b0a8ec9bd6c4692e068806e7
Tomer <tomergoldstgithub@gmail.com>
2016-07-02 08:42:59 +0300

    Initial commit

---
+++ app/src/main/java/com/tomergoldst/tooltipsdemo/MainActivity.java
@@ -0,0 +1,156 @@
+/*
+Copyright 2016 Tomer Goldstein
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package com.tomergoldst.tooltipsdemo;
+
+import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.tomergoldst.tooltips.ToolTip;
+import com.tomergoldst.tooltips.ToolTipsManager;
+
+
+public class MainActivity extends AppCompatActivity implements
+        ToolTipsManager.TipListener,
+        View.OnClickListener{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String TIP_TEXT = "Tip";
+
+    ToolTipsManager mToolTipsManager;
+    RelativeLayout mRootLayout;
+    RelativeLayout mParentLayout;
+    TextInputEditText mEditText;
+    TextView mTextView;
+
+    Button mAboveBtn;
+    Button mBelowBtn;
+    Button mLeftToBtn;
+    Button mRightToBtn;
+
+    RadioButton mAlignRight;
+    RadioButton mAlignLeft;
+    RadioButton mAlignCenter;
+
+    @ToolTip.Align int mAlign = ToolTip.ALIGN_CENTER;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRootLayout = (RelativeLayout) findViewById(R.id.root_layout);
+        mParentLayout = (RelativeLayout) findViewById(R.id.parent_layout);
+        mTextView = (TextView) findViewById(R.id.text_view);
+
+        mToolTipsManager = new ToolTipsManager(this);
+
+        mAboveBtn = (Button) findViewById(R.id.button_above);
+        mBelowBtn = (Button) findViewById(R.id.button_below);
+        mLeftToBtn = (Button) findViewById(R.id.button_left_to);
+        mRightToBtn = (Button) findViewById(R.id.button_right_to);
+
+        mAboveBtn.setOnClickListener(this);
+        mBelowBtn.setOnClickListener(this);
+        mLeftToBtn.setOnClickListener(this);
+        mRightToBtn.setOnClickListener(this);
+
+        mAlignCenter = (RadioButton) findViewById(R.id.button_align_center);
+        mAlignLeft = (RadioButton) findViewById(R.id.button_align_left);
+        mAlignRight = (RadioButton) findViewById(R.id.button_align_right);
+
+        mAlignCenter.setOnClickListener(this);
+        mAlignLeft.setOnClickListener(this);
+        mAlignRight.setOnClickListener(this);
+
+        mAlignCenter.setChecked(true);
+
+        mEditText = (TextInputEditText) findViewById(R.id.text_input_edit_text);
+
+
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        ToolTip.Builder builder = new ToolTip.Builder(this, mTextView, mRootLayout, TIP_TEXT, ToolTip.POSITION_ABOVE);
+        builder.setAlign(mAlign);
+        mToolTipsManager.show(builder.build());
+    }
+
+
+
+    @Override
+    public void onTipDismissed(View view) {
+        Log.d(TAG, "Dismissed view " + view.hashCode());
+    }
+
+    @Override
+    public void onClick(View view) {
+        String text = TextUtils.isEmpty(mEditText.getText()) ? TIP_TEXT : mEditText.getText().toString();
+        ToolTip.Builder builder;
+
+        switch (view.getId()){
+            case R.id.button_above:
+                mToolTipsManager.findAndDismiss(mTextView);
+                builder = new ToolTip.Builder(this, mTextView, mRootLayout, text, ToolTip.POSITION_ABOVE);
+                builder.setAlign(mAlign);
+                mToolTipsManager.show(builder.build());
+                break;
+            case R.id.button_below:
+                mToolTipsManager.findAndDismiss(mTextView);
+                builder = new ToolTip.Builder(this, mTextView, mRootLayout, text, ToolTip.POSITION_BELOW);
+                builder.setAlign(mAlign);
+                builder.setBackgroundColor(getResources().getColor(R.color.colorOrange));
+                mToolTipsManager.show(builder.build());
+                break;
+            case R.id.button_left_to:
+                mToolTipsManager.findAndDismiss(mTextView);
+                builder = new ToolTip.Builder(this, mTextView, mRootLayout, text, ToolTip.POSITION_LEFT_TO);
+                builder.setBackgroundColor(getResources().getColor(R.color.colorLightGreen));
+                builder.setTextColor(getResources().getColor(R.color.colorBlack));
+                builder.setGravity(ToolTip.GRAVITY_CENTER);
+                mToolTipsManager.show(builder.build());
+                break;
+            case R.id.button_right_to:
+                mToolTipsManager.findAndDismiss(mTextView);
+                builder = new ToolTip.Builder(this, mTextView, mRootLayout, text, ToolTip.POSITION_RIGHT_TO);
+                builder.setBackgroundColor(getResources().getColor(R.color.colorDarkRed));
+                builder.setTextColor(getResources().getColor(android.R.color.white));
+                mToolTipsManager.show(builder.build());
+                break;
+            case R.id.button_align_center:
+                mAlign = ToolTip.ALIGN_CENTER;
+                break;
+            case R.id.button_align_left:
+                mAlign = ToolTip.ALIGN_LEFT;
+                break;
+            case R.id.button_align_right:
+                mAlign = ToolTip.ALIGN_RIGHT;
+                break;
+        }
+    }
+}
---
+++ tooltips/src/main/java/com/tomergoldst/tooltips/ToolTipsManager.java
@@ -0,0 +1,227 @@
+/*
+Copyright 2016 Tomer Goldstein
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package com.tomergoldst.tooltips;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.SuppressLint;
+import android.graphics.Outline;
+import android.graphics.Point;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ToolTipsManager {
+
+    private static final String TAG = ToolTipsManager.class.getSimpleName();
+
+    private static final int DEFAULT_ANIM_DURATION = 400;
+
+    // Parameter for managing tip creation or reuse
+    private Map<Integer, View> mTipsMap = new HashMap<>();
+
+    private int mAnimationDuration;
+    private TipListener mListener;
+
+    public interface TipListener {
+        void onTipDismissed(View view);
+    }
+
+    public ToolTipsManager(){
+        mAnimationDuration = DEFAULT_ANIM_DURATION;
+    }
+
+    public ToolTipsManager(TipListener listener){
+        mAnimationDuration = DEFAULT_ANIM_DURATION;;
+        mListener = listener;
+    }
+
+    public View show(ToolTip toolTip) {
+        View tipView = create(toolTip);
+        if (tipView == null) {
+            return null;
+        }
+
+        // animate tip visibility
+        AnimationUtils.popup(tipView, mAnimationDuration).start();
+
+        return tipView;
+    }
+
+    private View create(ToolTip toolTip) {
+
+        if (toolTip.getAnchorView() == null) {
+            Log.e(TAG, "Unable to create a tip, anchor view is null");
+            return null;
+        }
+
+        if (toolTip.getRootView() == null) {
+            Log.e(TAG, "Unable to create a tip, root layout is null");
+            return null;
+        }
+
+        // only one tip is allowed near an anchor view at the same time, thus
+        // reuse tip if already exist
+        if (mTipsMap.containsKey(toolTip.getAnchorView().hashCode())) {
+            return mTipsMap.get(toolTip.getAnchorView().hashCode());
+        }
+
+        // init tip view parameters
+        TextView tipView = createTipView(toolTip);
+
+        // on RTL languages replace sides
+        if (UiUtils.isRtl()) {
+            switchToolTipSidePosition(toolTip);
+        }
+
+        // set tool tip background / shape
+        ToolTipBackgroundConstructor.setBackground(tipView, toolTip);
+
+        // add tip to root layout
+        toolTip.getRootView().addView(tipView);
+        // find where tip view is located currently on screen
+
+        // find where to position the tool tip
+        Point p = ToolTipCoordinatesFinder.getCoordinates(tipView, toolTip);
+
+        // move tip view to correct position
+        moveTipToCorrectPosition(tipView, p);
+
+        // set dismiss on click
+        tipView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss(view);
+            }
+        });
+
+        // store anchorView hashcode with tipView
+        int hashCode = toolTip.getAnchorView().hashCode();
+        tipView.setTag(hashCode);
+
+        // enter tip to map by 'anchorView' hash code
+        mTipsMap.put(hashCode, tipView);
+
+        return tipView;
+
+    }
+
+    private void moveTipToCorrectPosition(TextView tipView, Point p) {
+        Coordinates tipViewCoordinates = new Coordinates(tipView);
+        int translationX = p.x - tipViewCoordinates.left;
+        int translationY = p.y - tipViewCoordinates.top;
+        tipView.setTranslationX(!UiUtils.isRtl() ? translationX : -translationX);
+        tipView.setTranslationY(translationY);
+    }
+
+    @NonNull
+    private TextView createTipView(ToolTip toolTip) {
+        TextView tipView = new TextView(toolTip.getContext());
+        tipView.setTextColor(toolTip.getTextColor());
+        tipView.setText(toolTip.getMessage());
+        tipView.setVisibility(View.INVISIBLE);
+        tipView.setGravity(toolTip.getTextGravity());
+        setTipViewElevation(tipView, toolTip);
+        return tipView;
+    }
+
+    private void setTipViewElevation(TextView tipView, ToolTip toolTip) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (toolTip.getElevation() > 0) {
+                ViewOutlineProvider viewOutlineProvider = new ViewOutlineProvider() {
+                    @SuppressLint("NewApi")
+                    @Override
+                    public void getOutline(View view, Outline outline) {
+                        outline.setEmpty();
+                    }
+                };
+                tipView.setOutlineProvider(viewOutlineProvider);
+                tipView.setElevation(toolTip.getElevation());
+            }
+        }
+    }
+
+    private void switchToolTipSidePosition(ToolTip toolTip) {
+        if (toolTip.positionedLeftTo()) {
+            toolTip.setPosition(ToolTip.POSITION_RIGHT_TO);
+        } else if (toolTip.positionedRightTo()) {
+            toolTip.setPosition(ToolTip.POSITION_LEFT_TO);
+        }
+    }
+
+    public void setAnimationDuration(int duration){
+        mAnimationDuration = duration;
+    }
+
+    public boolean dismiss(View tipView) {
+        if (tipView != null && isVisible(tipView)) {
+            int key = (int) tipView.getTag();
+            mTipsMap.remove(key);
+            animateDismiss(tipView);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean dismiss(Integer key) {
+        return mTipsMap.containsKey(key) && dismiss(mTipsMap.get(key));
+    }
+
+    public View find(Integer key) {
+        if (mTipsMap.containsKey(key)) {
+            return mTipsMap.get(key);
+        }
+        return null;
+    }
+
+    public boolean findAndDismiss(final View anchorView) {
+        View view = find(anchorView.hashCode());
+        return view != null && dismiss(view);
+    }
+
+    public void clear() {
+        if (!mTipsMap.isEmpty()) {
+            for (Map.Entry<Integer, View> entry : mTipsMap.entrySet()) {
+                dismiss(entry.getValue());
+            }
+        }
+        mTipsMap.clear();
+    }
+
+    private void animateDismiss(final View view){
+        AnimationUtils.popout(view, mAnimationDuration, new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                if (mListener != null){
+                    mListener.onTipDismissed(view);
+                }
+            }
+        }).start();
+    }
+
+    public boolean isVisible(View tipView) {
+        return tipView.getVisibility() == View.VISIBLE;
+    }
+
+}
